fdb25e68dd499a3c9e3837dcce990ed369f8fa73
yunzhifeng01 <yunzhifeng01@126.com>
2024-09-04 17:16:18 +0800

    添加test04.java后提交

--- test02.java
+++ test02.java
@@ -1,6 +1,6 @@
-public  class  test01{
+public  class  test02{
 
-	public   static  void  main(String[])args{
+	public   static  void  main(String[]args){
 		System.out.print("hellworld");
 		System.out.print("添加一行代码");
 
---
+++ test04.java
@@ -0,0 +1,6 @@
+public class test04 {
+    public static void main(String[] args) {
+        System.out.print("hello   test04.java");
+    }
+
+}
